569ac397d262107bc99769a710d3428cf4b90e3b
Mario Eliezer Cisneros <marioeliezercb@outlook.es>
2017-11-25 12:27:03 -0600

    Diseño Login

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Medical thecnologies</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/stle.css">
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="input-group">
+			<input type="text" name="usuario" value="" placeholder="Usuario" class="form-control">
+			<input type="password" name="password" value="" placeholder="Usuario" class="form-control">
+			<input type="submit" name="Ingresar" value="Acceder">
+		</div>
+	</div>		
+</body>
+</html>
