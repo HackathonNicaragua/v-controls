0b754d628fae9a06f72b1a541587a36a924b3e80
Mario Eliezer Cisneros <marioeliezercb@outlook.es>
2017-11-26 04:43:24 -0600

    Retoques a la lista de pacientes

--- ListaExpedientes.php
+++ ListaExpedientes.php
@@ -83,10 +83,12 @@ session_destroy();
 </nav>
 
 <?php  
+$isregistro=false;
 function callWebService()
 {
   //Direccion del servidor donde se tienn los servicios
   if (strcmp(strtolower($_SESSION['niveles']),strtolower("registro"))==0){
+  $isregistro=true;
    return json_decode(file_get_contents('http://192.168.10.8/Hackathon2017/Hackathon2017/public/mostrarlistapacientes?Id_Usuario='.$_SESSION['id']),true);
 }
 else{
@@ -96,11 +98,19 @@ else{
 $pacientes='<div class="container"> <div class="list-group" id="lista"> ';
 $resul = callWebService();
 foreach($resul as $Expediente)
-{
+{  if ($isregistro) {
+  //Recordar cambiar el href
+   $pacientes.='<a href="Asignardoctor.php?n_exp='.$Expediente['Numero_Expediente']. '" class="list-group-item " >';
+    $pacientes.='<h4 id="paciente1" class="list-group-item-heading">'.$Expediente['Nombres'].' '.$Expediente['Apellidos'].'</h4>';
+    $pacientes.='<p class="list-group-item-text">'.$Expediente['Numero_Expediente'].'</p>';
+    $pacientes.='</a>';
+      } 
+else{
     $pacientes.='<a href="DatosPersonalesV.php?n_exp='.$Expediente['Numero_Expediente']. '" class="list-group-item " >';
     $pacientes.='<h4 id="paciente1" class="list-group-item-heading">'.$Expediente['Nombres'].' '.$Expediente['Apellidos'].'</h4>';
     $pacientes.='<p class="list-group-item-text">'.$Expediente['Numero_Expediente'].'</p>';
     $pacientes.='</a>';
+     }
 }
 $pacientes.='<div/><div/>';
 print_r ($pacientes);
